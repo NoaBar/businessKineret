b22fc592c86017b551533e78ca35d7cbdc013f40
Noah Bar <noahbar@gmail.com>
2018-02-16 02:11:12 +0100

    initial commit

---
+++ app/src/main/java/com/noah/businesskineret/MainActivity.java
@@ -0,0 +1,19 @@
+package com.noah.businesskineret;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView firstStep = (TextView) findViewById(R.id.firstStep);
+        firstStep.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+}
